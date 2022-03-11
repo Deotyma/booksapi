cc493021cd98951aab4577c0f8708f4a3007cb16
Ewa Kadziolka <kalistaewa@gmail.com>
2022-03-11 02:08:46 +0100

    nodemon to node  modified

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 const bodyParser = require('body-parser');
 const MongoDBbooks = require('../config/db.ts');
 const booksRoutes = require('./routes/booksRoutes');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
-    res.json({status: 200, result: "Deotyma's Books"});
+    res.json();
 });
 booksRoutes(app);
 app.listen(port, () => {
