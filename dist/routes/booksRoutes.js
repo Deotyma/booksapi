454d77e5cef19380219d4b6f5ffdda8f393c7b59
Ewa Kadziolka <kalistaewa@gmail.com>
2022-03-11 12:14:33 +0100

    Heroku

--- dist/routes/booksRoutes.js
+++ dist/routes/booksRoutes.js
@@ -1,16 +1,17 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 module.exports = (app) => {
     const Book = require('../models/booksModel');
-    app.post('/api/books/register', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.post('/api/books/register', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const data = {
             title: req.body.title,
             author: req.body.author,
@@ -21,16 +22,16 @@ module.exports = (app) => {
         const resultBook = yield book.save();
         res.json({ status: 200, result: resultBook });
     }));
-    app.get('/api/books/all', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.get('/api/books/all', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const allBooks = yield Book.find({});
         res.json({ status: 200, result: allBooks });
     }));
-    app.get('/api/books/:id', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.get('/api/books/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const id = req.params.id;
         const oneBook = yield Book.find({ _id: id });
         res.json({ status: 200, result: oneBook[0] });
     }));
-    app.put('/api/books/update/:id', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.put('/api/books/update/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const id = req.params.id;
         const data = {
             title: req.body.title,
@@ -40,7 +41,7 @@ module.exports = (app) => {
         const updatedBook = yield Book.updateOne({ _id: id }, data);
         res.json({ status: 200, result: updatedBook });
     }));
-    app.delete('/api/books/delete/:id', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.delete('/api/books/delete/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const id = req.params.id;
         const deletedBook = yield Book.deleteOne({ _id: id });
         res.json({ status: 200, result: deletedBook });
